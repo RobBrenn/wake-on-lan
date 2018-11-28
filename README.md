5bd470dc9051bc97c71b10423b35aa041879ac84
Rob B <robbrenn@hotmail.co.uk>
2018-11-28 22:10:29 +0000

    Initial commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+
+#Wake on Lan skill for Mycroft
+
+## Description
+A skill that uses the Wake On Lan protocol to wake up a device.
+
+## Examples
+* "Wake up"
+* "Turn On"
+
+## Setup
+1. Copy or clone this repository into mycroft's skills directory:
+
+2. Run the setup script while in the mycroft virtual environment
+```
+cd wake-on-lan
+sh ./setup.sh
+```
+
+## Credits
+RobBren
+remcohaszing https://github.com/remcohaszing/pywakeonlan
+
+
+## Category
+**IoT**
+
+## Tags
+#wake-on-lan
+#wake
---
+++ __init__.py
@@ -0,0 +1,49 @@
+# Copyright 2016 Mycroft AI, Inc.
+#
+# This file is part of Mycroft Core.
+#
+# Mycroft Core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mycroft Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+
+from adapt.intent import IntentBuilder
+
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.util.log import getLogger
+
+__author__ = 'RobBren'
+
+LOGGER = getLogger(__name__)
+
+
+class WakeOnLanSkill(MycroftSkill):
+    def __init__(self):
+        super(HelloWorldSkill, self).__init__(name="WakeOnLanSkill")
+
+#@intent_handler(IntentBuilder("").require("Wake").require("Device"))
+@intent_handler(IntentBuilder("").require("Wake")) #.require("Device"))
+ def handle_wake(self, message):
+
+     #handle having a device name
+     #device = message.data.get("Device")
+
+     address = self.settings.get("address")
+
+     #handle if device is not set
+     #if not url_rs
+
+     #handle invalid address
+
+     send_magic_packet(address)
+
+def create_skill():
+    return WakeOnLanSkill()
